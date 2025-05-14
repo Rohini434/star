fbfa988cb6b89dbbe59284c468e0597d76543c62
Rohini434 <rohinisach64@gmail.com>
2025-05-14 10:40:16 +0530

    mainn.py

--- mainn.py
+++ mainn.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("first app")
 st.write("created streamlit library")
+name=st.text_input("Enter your name: ")
+if st.button("submit"):
+  st.write("Hello, {name} Welcome to streamlit")
